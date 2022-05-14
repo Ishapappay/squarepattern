5c685e9e9f1526867d30bd5deb3fd768387d23a3
ishapappay <ishapappay@gmail.com>
2022-05-14 16:24:51 +0530

    Add project files.

---
+++ sample/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Program
+{
+    public static void Main(string[] args ) 
+    {
+        Console.WriteLine("asdfg");
+      
+   
+        int no = readno();
+        
+        for (int i = 0; i < no; i++)
+        {
+            for (int j = 0; j < no; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+            
+        }
+    }
+
+    public static int readno()
+    {
+        int no;
+        Console.WriteLine("Enter a number");
+        try
+        {
+            no = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+
+            Console.WriteLine("plesa");
+            no = readno();
+        }
+        return no;
+    }
+}
