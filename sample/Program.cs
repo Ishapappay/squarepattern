86734dd38743629d39ae322d6064fa3e766df135
ishapappay <ishapappay@gmail.com>
2022-05-14 18:34:51 +0530

    er

--- sample/Program.cs
+++ sample/Program.cs
@@ -6,16 +6,40 @@
       
    
         int no = readno();
-        
-        for (int i = 0; i < no; i++)
+
+        for(int i = 1; i <= no; i++)
         {
-            for (int j = 0; j < no; j++)
+            for (int k = i; k < no; k++)
+            {
+                Console.Write(" ");
+
+            }
+            for (int j = 1; j < 2 * i; j++)
             {
                 Console.Write("*");
             }
-            Console.WriteLine("");
-            
+
+            Console.WriteLine();            
         }
+
+        // for(int i = 0; i < no; i++)
+      //  {
+      //     for(int j = no; j > i; j--)
+          //  {
+       //         Console.Write("*");
+          //  }
+         //  Console.WriteLine("");
+      //  }
+        
+        //for (int i = 0; i <= no; i++)
+        //{
+        //    for (int j = 0; j < i; j++)
+        //    {
+        //        Console.Write("*");
+        //    }
+        //    Console.WriteLine("");
+            
+        //}
     }
 
     public static int readno()
